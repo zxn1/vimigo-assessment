c755d722cbddc461db23d0561e49ade7a0bc6aaa
Muhd Jamil Husni <maintanance1991@gmail.com>
2023-02-01 02:52:55 +0800

    v2. Authentication
    1. allow Staff to makes authentication
    2. login/register staff account

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,5 +8,42 @@ use Validator;
 
 class AuthController extends Controller
 {
-    
+    //register
+    public function register(Request $request){ 
+
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required',
+            'c_password'=>'required|same:password'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),202);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        $resArr = [];
+        $resArr['token'] = $user->createToken('vimigoApp')->accessToken;
+        $resArr['name'] = $user->name;
+        
+        return response()->json($resArr,200);  
+    }
+
+    public function login(Request $request){ 
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+            $user = Auth::user();
+            $resArr = [];
+            $resArr['token'] = $user->createToken('vimigoApp')->accessToken;
+            $resArr['name'] = $user->name;
+            
+            return response()->json($resArr,200);
+
+        }else{
+            return response()->json(['error'=>'Failed to login! Wrong authentication details!'],203);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* Authentication */
+Route::post('/register',[AuthController::class,'register']); //University Staff Register - endpoint : localhost:8000/api/register
+Route::post('/login',[AuthController::class,'login']); //University Staff Login - endpoint : localhost:8000/api/login
+Route::get('/login',[AuthController::class,'login'])->name('login');
+/* End Authentication */
+
+
+/* below details - must include in header to access secured route */
+//no   //key                //value
+//1    //Accept             //application/json
+//2    //Authorization      //Bearer [token]
+
+/* Secured route - need authenticated first */
+Route::middleware('auth:api')->get('/user', function(Request $request)
+{
+    return $request->user();
+});
+/* end secured route */
